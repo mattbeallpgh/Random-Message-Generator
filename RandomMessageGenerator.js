9aaf131b2303ef29ee97e7174ffce5f29102684e
Matt Beall <mattbeall@gmail.com>
2021-12-23 14:24:03 -0500

    Added JS file with message parts object

---
+++ RandomMessageGenerator.js
@@ -0,0 +1,10 @@
+//contains the message parts to be pieced together
+let messageParts = {
+    partsOne: ['Did you know ', 'How strange that ', 'A mystical wizard cast a spell ensuring ', 'A pack of wild dogs will say that ', 'Mojo Jojo has claimed'],
+    partsTwo: ['all dogs go to heaven ', 'rats control the government ', 'happiness prevails ', 'whales cannot eat gummy candy ', 'portals to the netherworld are painted orange '],
+    partsThree: ['at least once or twice a generation.', 'even if most of us will never know.', 'in certain lands.', ', and only fools think otherwise.', 'during certain lunar cycles.'],
+ };
+ 
+
+
+
